06c2844d4c19d330403ed7a99247394c27ab5c3b
Marut <marut.dhungana@gmail.com>
2021-05-16 20:19:32 +0545

    Added sum code

---
+++ NumArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayStuffs
+{
+    class NumArray
+    {
+        public void sumArray(int[] array)
+        {
+            int total = array.Sum();
+            Console.WriteLine("Sum of array is  " + total);
+
+        }
+        
+        
+    }
+}
